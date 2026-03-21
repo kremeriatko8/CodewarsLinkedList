f93f1f373add0f1dc66caf9d08bc496e677a9193
kremeriatko8 <sofia.kremer08@gmail.com>
2026-03-21 23:47:27 +0200

    The fifth task was to take the head of one linked list, delete it, and put it at the beginning of another and create a new Context.

---
+++ Linked_Lists_Move_Node/solution.py
@@ -0,0 +1,17 @@
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class Context(object):
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
+
+def move_node(source, dest):
+    if source is None:
+        raise ValueError()
+    new_source = source.next
+    node_move = source
+    node_move.next = dest
+    return Context(new_source, node_move)

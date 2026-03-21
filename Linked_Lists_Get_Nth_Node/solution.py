56204382ff864a0b591073110216cb833af25e6d
kremeriatko8 <sofia.kremer08@gmail.com>
2026-03-21 23:17:37 +0200

    the fourth problem from the list - the point is to find the node by index - I created a variable count which was like a flag whether I found the desired index or not

---
+++ Linked_Lists_Get_Nth_Node/solution.py
@@ -0,0 +1,15 @@
+class Node(object):
+    """Node class for reference"""
+    def __init__(self, data, next=None):
+        self.data = data
+        self.next = next
+
+def get_nth(node, index):
+    cur_node = node
+    count = 0
+    while cur_node is not None:
+        if count == index:
+            return cur_node
+        count += 1
+        cur_node = cur_node.next
+    raise ArgumentException()
